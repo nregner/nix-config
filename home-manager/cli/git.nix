c511f502dfffe7603742ec3caca8182ce354b169
Nathan Regner <nathanregner@gmail.com>
2023-08-12 18:34:39 -0600

    Update git config

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -4,5 +4,8 @@
     userName = "Nathan Regner";
     userEmail = "nathanregner@gmail.com";
     lfs.enable = true;
+    difftastic.enable = true;
+    extraConfig = { push = { autoSetupRemote = true; }; };
   };
+  programs.lazygit.enable = true;
 }
