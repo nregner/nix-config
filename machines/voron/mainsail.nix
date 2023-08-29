6a4d72be23751d4649eea64b4513ee4f3748fa72
Nathan Regner <nathanregner@gmail.com>
2023-08-28 20:21:06 -0600

    Add ustreamer

--- machines/voron/mainsail.nix
+++ machines/voron/mainsail.nix
@@ -1,4 +1,23 @@
-{
+{ pkgs, ... }: {
   services.mainsail = { enable = true; };
   services.nginx = { clientMaxBodySize = "1G"; };
+
+  systemd.services.ustreamer = {
+    description = "Mainsail webcam stream";
+    wantedBy = [ "multi-user.target" ];
+    script = with pkgs; ''
+      ${ustreamer}/bin/ustreamer --device /dev/video0 --host 0.0.0.0 --port 81
+    '';
+  };
+
+  # restart ustreamer when webcam is plugged in
+  services.udev.extraRules = ''
+    ACTION=="add", ENV{SUBSYSTEM}=="video4linux", RUN+="${pkgs.bash} -c 'systemctl restart ustreamer.service'"
+  '';
+
+  networking.firewall = let ports = [ 80 81 ];
+  in {
+    allowedTCPPorts = ports;
+    allowedUDPPorts = ports;
+  };
 }
