728f5bc834163f93b5beae211265b870cc4c3b5b
Nathan Regner <nathanregner@gmail.com>
2023-09-21 23:29:46 -0600

    feat(nvim): add "gx" mapping to open links

--- home-manager/nvim/lua/user/keybinds.lua
+++ home-manager/nvim/lua/user/keybinds.lua
@@ -1,5 +1,14 @@
+-- map helper
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
 -- https://github.com/neovim/neovim/issues/23093
-vim.keymap.set('n', '<leader>ex', function()
+map('n', '<leader>ex', function()
     for _, ui in pairs(vim.api.nvim_list_uis()) do
         if ui.chan and not ui.stdout_tty then
             vim.fn.chanclose(ui.chan)
@@ -7,7 +16,7 @@ vim.keymap.set('n', '<leader>ex', function()
     end
 end)
 
-vim.keymap.set('n', '<leader>sv', function()
+map('n', '<leader>sv', function()
     -- source: https://github.com/creativenull
     for name, _ in pairs(package.loaded) do
         if name:match('^user') then
@@ -18,3 +27,13 @@ vim.keymap.set('n', '<leader>sv', function()
     dofile(vim.env.MYVIMRC)
     vim.notify("Config reloaded", vim.log.levels.INFO)
 end)
+
+-- URL handling
+-- source: https://sbulav.github.io/vim/neovim-opening-urls/
+if vim.fn.has("mac") == 1 then
+    map("", "gx", '<Cmd>call jobstart(["open", expand("<cfile>")], {"detach": v:true})<CR>', {})
+elseif vim.fn.has("unix") == 1 then
+    map("", "gx", '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>', {})
+else
+    map("", "gx", '<Cmd>lua print("Error: gx is not supported on this OS!")<CR>')
+end
