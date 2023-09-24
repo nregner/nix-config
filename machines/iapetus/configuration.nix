b5e230eec65febfc656dd5f67e57a131eca708b0
Nathan Regner <nathanregner@gmail.com>
2023-09-23 20:49:56 -0600

    feat(iapetus): enable NTFS + sync VM config

--- machines/iapetus/configuration.nix
+++ machines/iapetus/configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems =
-    lib.mkForce [ "vfat" "fat32" "exfat" "ext4" "btrfs" ];
+    lib.mkForce [ "vfat" "fat32" "exfat" "ext4" "btrfs" "ntfs" ];
 
   # Networking
   networking.hostName = "iapetus";
