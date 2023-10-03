7fe7d87c64650760c196ffa2f6a07cbeea91c3e5
Nathan Regner <nathanregner@gmail.com>
2023-10-02 20:17:30 -0600

    Update netdata

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,23 @@
   route53-ddns = pkgs.unstable.callPackage ./route53-ddns { };
   # jetbrains-toolbox = pkgs.unstable.callPackage ./jetbrains-toolbox.nix { };
 
+  # https://github.com/netdata/netdata/releases
+  netdata-latest = pkgs.unstable.netdata.overrideAttrs (oldAttrs: rec {
+    version = "1.42.4";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "netdata";
+      repo = "netdata";
+      rev = "v${version}";
+      hash = "sha256-8L8PhPgNIHvw+Dcx2D6OE8fp2+GEYOc9wEIoPJSqXME=";
+      fetchSubmodules = true;
+    };
+
+    # FIXME: Typo in nixpkgs
+    # enableParallelBuild = true;
+    enableParallelBuilding = true;
+  });
+
   # cross-compile heavy ARM on dependencies on more powerful x86 machines
   # TODO: Something more generic/flexible
   cross = import ./cross.nix {
