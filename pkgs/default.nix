1d0a4ae33945bfcc60081b7c59cb520278be6905
Nathan Regner <nathanregner@gmail.com>
2023-10-02 19:58:34 -0600

    chore(flake): update inputs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,23 +6,6 @@
   route53-ddns = pkgs.unstable.callPackage ./route53-ddns { };
   # jetbrains-toolbox = pkgs.unstable.callPackage ./jetbrains-toolbox.nix { };
 
-  # https://github.com/netdata/netdata/releases
-  netdata-latest = pkgs.unstable.netdata.overrideAttrs (oldAttrs: rec {
-    version = "1.42.4";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "netdata";
-      repo = "netdata";
-      rev = "v${version}";
-      hash = "sha256-8L8PhPgNIHvw+Dcx2D6OE8fp2+GEYOc9wEIoPJSqXME=";
-      fetchSubmodules = true;
-    };
-
-    # FIXME: Typo in nixpkgs
-    # enableParallelBuild = true;
-    enableParallelBuilding = true;
-  });
-
   # https://github.com/realthunder/FreeCAD/tree/LinkMerge
   freecad-link = pkgs.unstable.freecad.overrideAttrs (oldAttrs: {
     pname = "${oldAttrs.pname}-link";
