435923e5dc1e94b9921d0f77a1e3d70d744a1785
Nathan Regner <nathanregner@gmail.com>
2023-10-02 22:13:15 -0600

    fix: grant full attic token permissions

--- scripts/attic-login.sh
+++ scripts/attic-login.sh
@@ -2,10 +2,13 @@ ssh sagittarius -- \
   sudo atticd-atticadm make-token \
     --sub 'admin' \
     --validity '1y' \
-    --create-cache 'default' \
     --push 'default' \
     --pull 'default' \
     --delete 'default' \
+    --create-cache 'default' \
+    --configure-cache 'default' \
+    --configure-cache-retention 'default' \
+    --destroy-cache 'default' \
   | tail -n 1 \
   | tr -d '\r' \
   | xargs attic login 'default' 'http://sagittarius:8080'
