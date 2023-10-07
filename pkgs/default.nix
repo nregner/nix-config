25eddce207b97e8b3ac204a4d4b262787fe8ccad
Nathan Regner <nathanregner@gmail.com>
2023-10-06 22:04:01 -0600

    feat(nixos): use ccache

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,11 +10,40 @@
 
   # jetbrains-toolbox = pkgs.unstable.callPackage ./jetbrains-toolbox.nix { };
 
+  # # https://github.com/realthunder/FreeCAD/tree/LinkMerge
+  # freecad-link = (pkgs.unstable.freecad.override {
+  #   stdenv = pkgs.unstable.ccacheStdenv;
+  # }).overrideAttrs (oldAttrs: {
+  #   pname = "${oldAttrs.pname}-link";
+  #   version = inputs.freecad.rev;
+  #   src = inputs.freecad;
+  # });
+
   # https://github.com/realthunder/FreeCAD/tree/LinkMerge
-  freecad-link = pkgs.unstable.freecad.overrideAttrs (oldAttrs: {
-    pname = "${oldAttrs.pname}-link";
+  freecad-link2 = pkgs.unstable.ccacheStdenv.mkDerivation {
+    pname = "freecad-link";
     version = inputs.freecad.rev;
     src = inputs.freecad;
+  };
+
+  # # https://github.com/realthunder/FreeCAD/tree/LinkMerge
+  # freecad-link = ((pkgs.unstable.libsForQt5.callPackage ./freecad.nix {
+  #   inputs = { inherit (inputs) freecad; };
+  #   boost = pkgs.unstable.python3Packages.boost;
+  #   inherit (pkgs.unstable.python3Packages)
+  #     gitpython matplotlib pivy ply pycollada pyside2 pyside2-tools python
+  #     pyyaml scipy shiboken2;
+  #   })(.override { stdenv = pkgs.unstable.ccacheStdenv;
+  # }).overrideAttrs( prev: {
+  #   nativeBuildInputs = [ ccacheStdenv.cc] ++ prev.nativeBuildInputs;
+  # }))
+
+  # https://github.com/realthunder/FreeCAD/tree/LinkMerge
+  freecad-link = let inherit (pkgs.unstable) freecad ccacheStdenv;
+  in (freecad.override { stdenv = ccacheStdenv; }).overrideAttrs (prev: {
+    version = inputs.freecad.rev;
+    src = inputs.freecad;
+    nativeBuildInputs = [ ccacheStdenv.cc ] ++ prev.nativeBuildInputs;
   });
 
   # cross-compile heavy ARM on dependencies on more powerful x86 machines
@@ -25,3 +54,4 @@
     crossSystem = "aarch64-multiplatform";
   };
 }
+
