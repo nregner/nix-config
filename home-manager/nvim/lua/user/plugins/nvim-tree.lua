3ccbd8da116107017892c0a3a008ac5244d54088
Nathan Regner <nathanregner@gmail.com>
2023-10-14 11:32:03 -0600

    feat(nvim): update nvim-tree bindings

--- home-manager/nvim/lua/user/plugins/nvim-tree.lua
+++ home-manager/nvim/lua/user/plugins/nvim-tree.lua
@@ -2,4 +2,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({})
+
+local api = require('nvim-tree.api')
+vim.keymap.set('n', '<leader>ft', function()
+    print('find')
+    api.tree.open({ find_file = true, })
+end, {})
