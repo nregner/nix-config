e995afbf345e6ee8b2be3309d57a0f0ec78f0280
Nathan Regner <nathanregner@gmail.com>
2023-10-13 20:41:10 -0600

    fix(nixos): fix incorrect flake source path

--- nixos/common/global/nix.nix
+++ nixos/common/global/nix.nix
@@ -45,7 +45,7 @@
 
   # keep a reference to the flake source that was used to build
   # https://www.reddit.com/r/NixOS/comments/16t2njf/small_trick_for_people_using_nixos_with_flakes
-  environment.etc."nixos/flake".source = ../..;
+  environment.etc."nixos/flake".source = self.outPath;
   system.nixos.tags = [ self.sourceInfo.shortRev or "dirty" ];
 
   # show config changes on switch
