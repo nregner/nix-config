1c096d2bbb15bd74d0ecd820e44c46866351b2de
Nathan Regner <nathanregner@gmail.com>
2023-10-26 17:04:14 -0600

    feat(iapetus): enable flatpak

--- nixos/iapetus/configuration.nix
+++ nixos/iapetus/configuration.nix
@@ -103,6 +103,8 @@
       true; # Open ports in the firewall for Source Dedicated Server
   };
 
+  services.flatpak.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
