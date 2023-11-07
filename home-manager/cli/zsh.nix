25685f12c5797d76cf819173776f9eded62f53cb
Nathan Regner <nathanregner@gmail.com>
2023-11-07 14:15:54 -0700

    feat(home): enable starship docker context

--- home-manager/cli/zsh.nix
+++ home-manager/cli/zsh.nix
@@ -58,9 +58,10 @@
     settings = {
       # Move directory to the second line
       format = "$all$directory$character";
-      package = { disabled = true; };
-      aws = { disabled = true; };
-      nix_shell = { disabled = true; };
+      package.disabled = true;
+      aws.disabled = true;
+      nix_shell.disabled = true;
+      docker_context = { only_with_files = false; };
       custom.direnv = {
         detect_files = [ ".envrc" ];
         when = ''[[ $(direnv status) =~ " Found RC allowed true " ]]'';
