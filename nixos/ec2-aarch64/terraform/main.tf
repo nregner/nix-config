e4b14f8a972eea93ccb27db01dd6cc0a5467eb6b
Nathan Regner <nathanregner@gmail.com>
2023-12-03 18:41:09 -0700

    feat(infra): use spot instance for aarch64 builder
    This reverts commit 13ecf1b264aa525f43a8f1d3648fc5796f4f653d.

--- nixos/ec2-aarch64/terraform/main.tf
+++ nixos/ec2-aarch64/terraform/main.tf
@@ -16,10 +16,29 @@ data "terraform_remote_state" "kms" {
   }
 }
 
-resource "aws_instance" "aarch64_builder" {
+# resource "aws_instance" "aarch64_builder" {
+#   # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/amazon-ec2-amis.nix#L585
+#   # ami                  = "ami-0d0979d889078d036"
+#   ami                  = "ami-07ea01c1c5c66b8f7"
+#   instance_type        = "c7g.4xlarge"
+#   key_name             = aws_key_pair.ssh.key_name
+#   iam_instance_profile = aws_iam_instance_profile.nix_builder.name
+#   private_dns_name_options {
+#     hostname_type = "resource-name"
+#   }
+#   ebs_block_device {
+#     device_name           = "/dev/xvda"
+#     volume_size           = 64
+#     delete_on_termination = false
+#   }
+# }
+
+resource "aws_spot_instance_request" "aarch64_builder" {
+  spot_type = "one-time"
   # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/amazon-ec2-amis.nix#L585
-  ami                  = "ami-0d0979d889078d036"
-  instance_type        = "c7g.4xlarge"
+  ami = "ami-0d0979d889078d036"
+  # ami                  = "ami-07ea01c1c5c66b8f7"
+  instance_type        = "c7g.8xlarge"
   key_name             = aws_key_pair.ssh.key_name
   iam_instance_profile = aws_iam_instance_profile.nix_builder.name
   private_dns_name_options {
@@ -28,13 +47,13 @@ resource "aws_instance" "aarch64_builder" {
   ebs_block_device {
     device_name           = "/dev/xvda"
     volume_size           = 64
-    delete_on_termination = false
+    delete_on_termination = true
   }
 }
 
 resource "aws_key_pair" "ssh" {
   key_name   = "ec2-aarch64-nix"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDk7uVEehfyhAZUvZbvH5Kw85MzLyYqVdTOMBXsmBeLx"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJN0UxPvRjkqYdq8OFtzO/borc4lU4QNYSJiGhgx3MkI"
 }
 
 resource "aws_iam_instance_profile" "nix_builder" {
