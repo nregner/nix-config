3fa957143a8255b6c1241a9307563ac59850eb0b
Nathan Regner <nathanregner@gmail.com>
2023-12-17 18:35:20 -0700

    feat(home): add common cli packages

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   imports = [
     #
     ./fzf.nix
@@ -6,6 +6,33 @@
     ./nix.nix
     ./tmux.nix
     ./zsh.nix
+  ];
+
+  home.packages = with pkgs.unstable; [
+    # text manipulation
+    gawk
+    gnused
+    jq
+    ripgrep
+    vim
+
+    # filesystem
+    fd
+    file
+    pv
+    rsync
+    tree
+    which
+
+    # archive formats
+    gnutar
+    unzip
+    xz
+    zip
+    zstd
+
+    # system monitoring
+    htop-vim
   ];
 }
 
