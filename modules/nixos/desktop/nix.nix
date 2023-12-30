2f73e3e876f74f4afc643aa778763fb29f4a327d
Nathan Regner <nathanregner@gmail.com>
2023-12-30 14:29:38 -0700

    fix: set nix-path to flake channel

--- modules/nixos/desktop/nix.nix
+++ modules/nixos/desktop/nix.nix
@@ -12,17 +12,14 @@
       nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
     };
 
-    # https://discourse.nixos.org/t/do-flakes-also-set-the-system-channel/19798
-    # pin system channels to flake inputs
-    nixPath = [
-      "nixpkgs=/etc/nix/flake-channels/nixpkgs"
-      "nixpkgs-unstable=/etc/nix/flake-channels/nixpkgs-unstable"
-    ];
-
     settings = {
       # keep build dependencies for direnv GC roots
       keep-derivations = true;
       keep-outputs = true;
+
+      # https://discourse.nixos.org/t/do-flakes-also-set-the-system-channel/19798
+      # pin system channels to flake inputs
+      nix-path = "nixpkgs=/etc/nix/flake-channels/nixpkgs";
     };
   };
 }
