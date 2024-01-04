7d949979dc68d294c81b340246ef57bcf82dc0a2
Nathan Regner <nathan.regner@clickbank.com>
2024-01-04 12:10:18 -0700

    feat(home): use fd for fzf file search

--- modules/home-manager/base/fzf.nix
+++ modules/home-manager/base/fzf.nix
@@ -1,21 +1,16 @@
 {
-  programs.fzf = {
+  programs.fzf = rec {
     enable = true;
     enableZshIntegration = true;
-    # https://github.com/catppuccin/fzf
-    colors = {
-      "bg" = "#1e1e2e";
-      "bg+" = "#313244";
-      "fg" = "#cdd6f4";
-      "fg+" = "#cdd6f4";
-      "header" = "#f38ba8";
-      "hl" = "#f38ba8";
-      "hl+" = "#f38ba8";
-      "info" = "#cba6f7";
-      "marker" = "#f5e0dc";
-      "pointer" = "#f5e0dc";
-      "prompt" = "#cba6f7";
-      "spinner" = "#f5e0dc";
-    };
+    # https://github.com/sharkdp/fd#using-fd-with-fzf
+    defaultCommand = "fd --hidden --follow --exclude .git";
+    fileWidgetCommand = defaultCommand;
+    defaultOptions = [
+      "--ansi"
+      # https://github.com/catppuccin/fzf
+      "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
+      "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
+      "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+    ];
   };
 }
