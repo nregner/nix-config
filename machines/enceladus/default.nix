a97a1d42e1fbb0ebcd2099f47bf0b0dce9557c18
Nathan Regner <nathanregner@gmail.com>
2024-01-07 12:10:26 -0700

    feat(enceladus): enable nix automatic gc

--- machines/enceladus/default.nix
+++ machines/enceladus/default.nix
@@ -2,7 +2,7 @@
   system = "aarch64-darwin";
 
   darwinConfigurations."Nathans-MacBook-Pro".modules =
-    [ ../../modules/darwin/default.nix ./configuration.nix ];
+    [ ../../modules/darwin /configuration.nix ];
 
   homeConfigurations."nregner".modules =
     [ ../../modules/home-manager/desktop ./home.nix ];
