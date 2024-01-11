b0a2676c04de5f194b6d4c8b294cfe266133dbfb
Nathan Regner <nathanregner@gmail.com>
2024-01-10 17:25:40 -0700

    feat: add klipper input-shaper packages

---
+++ pkgs/klipper/calibrate-shaper.nix
@@ -0,0 +1,8 @@
+{ klipper, python3, writeShellApplication, }:
+writeShellApplication {
+  name = "calibrate_shaper";
+  runtimeInputs = [ (python3.withPackages (ps: [ ps.numpy ps.matplotlib ])) ];
+  text = ''
+    python3 ${klipper.src}/scripts/calibrate_shaper.py "$@"
+  '';
+}
