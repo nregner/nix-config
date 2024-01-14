311b1c53d23da976812a3746546fe8e01e08babf
Nathan Regner <nathanregner@gmail.com>
2024-01-13 20:49:06 -0700

    build: update inputs

--- modules/home-manager/desktop/alacritty.nix
+++ modules/home-manager/desktop/alacritty.nix
@@ -1,8 +1,8 @@
-{ inputs, ... }: {
+{ config, inputs, pkgs, ... }: {
   programs.alacritty = {
     enable = true;
     settings = {
-      import = [ "${inputs.catppuccin-alacritty}/catppuccin-mocha.yml" ];
+      import = [ "${config.xdg.configHome}/alacritty/theme.yml" ];
       selection = { save_to_clipboard = true; };
       window = { dynamic_padding = true; };
       font = {
@@ -15,4 +15,11 @@
       env = { TERM = "alacritty"; };
     };
   };
+
+  xdg.configFile."alacritty/theme.yml".source =
+    pkgs.runCommand "alacritty-theme" {
+      nativeBuildInputs = [ pkgs.remarshal ];
+    } ''
+      remarshal ${inputs.catppuccin-alacritty}/catppuccin-mocha.toml -if toml -of yaml -o $out;
+    '';
 }
--- modules/home-manager/desktop/cli/k9s.nix
+++ modules/home-manager/desktop/cli/k9s.nix
@@ -1,7 +1,7 @@
 { inputs, config, ... }: {
   programs.k9s = {
     enable = true;
-    skin =
-      config.lib.formats.fromYAML "${inputs.catppuccin-k9s}/dist/mocha.yml";
+    skin = config.lib.formats.fromYAML
+      "${inputs.catppuccin-k9s}/dist/catppuccin-mocha.yaml";
   };
 }
