b2da0a236947fda2440999bb274287e306d21fba
Nathan Regner <nathanregner@gmail.com>
2024-01-20 15:11:59 -0700

    Works but waaaay too slow

--- machines/print-farm/hardware/raspberry-pi-zero2w.nix
+++ machines/print-farm/hardware/raspberry-pi-zero2w.nix
@@ -1,32 +1,41 @@
 { modulesPath, pkgs, lib, ... }: {
   imports = [
-    # "${modulesPath}/profiles/minimal.nix"
-    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
-    ./sd-image.nix
+    "${modulesPath}/profiles/minimal.nix"
+    # "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+    # ./sd-image.nix
+  ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      qemu = prev.qemu.override {
+        gtkSupport = false;
+        pipewireSupport = false;
+      };
+    })
   ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
 
   zramSwap.enable = true;
 
-  sdImage = {
-    # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low on space.
-    compressImage = true;
-    imageName = "zero2.img";
+  # sdImage = {
+  #   # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low on space.
+  #   compressImage = true;
+  #   imageName = "zero2.img";
 
-    extraFirmwareConfig = {
-      # Give up VRAM for more Free System Memory
-      # - Disable camera which automatically reserves 128MB VRAM
-      start_x = 0;
-      # - Reduce allocation of VRAM to 16MB minimum for non-rotated (32MB for rotated)
-      gpu_mem = 16;
+  #   extraFirmwareConfig = {
+  #     # Give up VRAM for more Free System Memory
+  #     # - Disable camera which automatically reserves 128MB VRAM
+  #     start_x = 0;
+  #     # - Reduce allocation of VRAM to 16MB minimum for non-rotated (32MB for rotated)
+  #     gpu_mem = 16;
 
-      # Configure display to 800x600 so it fits on most screens
-      # * See: https://elinux.org/RPi_Configuration
-      hdmi_group = 2;
-      hdmi_mode = 8;
-    };
-  };
+  #     # Configure display to 800x600 so it fits on most screens
+  #     # * See: https://elinux.org/RPi_Configuration
+  #     hdmi_group = 2;
+  #     hdmi_mode = 8;
+  #   };
+  # };
 
   # Keep this to make sure wifi works
   hardware.enableRedistributableFirmware = lib.mkForce false;
