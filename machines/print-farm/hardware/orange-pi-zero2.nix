1417817d009e244a52f13be9691a36dd0605aaa7
Nathan Regner <nathanregner@gmail.com>
2024-01-27 17:00:16 -0700

    fix: disable wifi power save?

--- machines/print-farm/hardware/orange-pi-zero2.nix
+++ machines/print-farm/hardware/orange-pi-zero2.nix
@@ -1,5 +1,13 @@
-{ inputs, lib, ... }: {
+{ inputs, pkgs, lib, ... }: {
   imports = [ inputs.orangepi-nix.nixosModules.zero2 ];
   nixpkgs.overlays = [ inputs.orangepi-nix.overlays.default ];
   nixpkgs.hostPlatform = lib.mkForce "aarch64-linux";
+
+  # https://unix.stackexchange.com/questions/593573/how-to-turn-off-wireless-power-management-permanently-using-systemd-networkd
+  systemd.services.wlan-always-on = {
+    description = "Keep wireless device %i from sleeping";
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = { ExecStart = "${pkgs.iw}/bin/iw %i set power_save off"; };
+  };
 }
