db4d3cbbdafcd4837fa33c5145943ef28745ef97
Nathan Regner <nathanregner@gmail.com>
2024-02-01 22:25:47 -0700

    fix: klipper calibrate-shaper package path

--- pkgs/klipper/default.nix
+++ pkgs/klipper/default.nix
@@ -1,5 +1,5 @@
 { pkgs, lib }:
 lib.recurseIntoAttrs {
   flash-rp2040 = pkgs.callPackage ./rp2040.nix { };
-  calibrate-shaper = pkgs.callPackage ./rp2040.nix { };
+  calibrate-shaper = pkgs.callPackage ./calibrate-shaper.nix { };
 }
