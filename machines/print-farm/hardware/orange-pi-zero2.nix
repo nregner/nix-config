b3e36d06a78d46af19fca7f020b10bad20af3ff3
Nathan Regner <nathanregner@gmail.com>
2024-02-10 15:55:39 -0700

    fixup! update orangepi-nix

--- machines/print-farm/hardware/orange-pi-zero2.nix
+++ machines/print-farm/hardware/orange-pi-zero2.nix
@@ -1,5 +1,8 @@
-{ inputs, lib, ... }: {
+{ inputs, pkgs, lib, ... }: {
   imports = [ inputs.orangepi-nix.nixosModules.zero2 ];
   nixpkgs.overlays = [ inputs.orangepi-nix.overlays.default ];
   nixpkgs.hostPlatform = lib.mkForce "aarch64-linux";
+
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor
+    inputs.orangepi-nix.packages.x86_64-linux.pkgsCross.linux-6_6-rk35xx);
 }
