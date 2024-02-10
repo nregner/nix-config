30d3079efc0fffff4b61188b5e366a15aa588a56
Nathan Regner <nathanregner@gmail.com>
2024-02-10 00:13:36 -0700

    tweak(shell): remove colmena

--- shells.nix
+++ shells.nix
@@ -1,15 +1,13 @@
 { inputs, pkgs }: {
-  default =
-    pkgs.mkShell { packages = with pkgs.unstable; [ colmena sops terraform ]; };
+  default = pkgs.mkShell { packages = with pkgs.unstable; [ sops terraform ]; };
 
   bootstrap = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs.unstable; [ nix git ];
-    packages = with pkgs.unstable;
-      [
-        inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.home-manager
-      ];
+    packages = [
+      inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.home-manager
+    ];
   };
 
   rust = pkgs.mkShell { packages = with pkgs; [ cargo rustfmt ]; };
