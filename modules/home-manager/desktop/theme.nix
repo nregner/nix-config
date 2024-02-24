8b51cb5baab3e015a677fe3289b957b0c37575c5
Nathan Regner <nathanregner@gmail.com>
2024-02-24 13:33:22 -0700

    fix(home): fix catppuccin gtk 4.0 theme

--- modules/home-manager/desktop/theme.nix
+++ modules/home-manager/desktop/theme.nix
@@ -1,8 +1,40 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs.unstable;
     [
       # nerdfonts is large - just use a subset
       (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      # nix build .\#homeConfigurations.nregner@iapetus.config.gtk.theme.package
+      # ls result/share/themes
+      name = "Catppuccin-Mocha-Compact-Blue-Dark";
+      package = pkgs.unstable.catppuccin-gtk.override {
+        accents = [ "blue" ];
+        size = "compact";
+        tweaks = [ "rimless" ];
+        variant = "mocha";
+      };
+    };
+  };
+
+  home.pointerCursor = {
+    name = "Catppuccin-Mocha-Dark-Cursors";
+    package = pkgs.unstable.catppuccin-cursors.mochaDark;
+    size = 24;
+    gtk.enable = true;
+  };
+
+  # https://github.com/catppuccin/gtk
+  xdg.configFile = {
+    "gtk-4.0/assets".source =
+      "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source =
+      "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source =
+      "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 }
