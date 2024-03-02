2a8b1e56fd9a2753d3281bebb34934edc68f82da
Nathan Regner <nathanregner@gmail.com>
2024-03-01 19:23:06 -0700

    fix: conflicting mealie service name

--- machines/sagittarius/mealie.nix
+++ machines/sagittarius/mealie.nix
@@ -1,17 +1,18 @@
 { inputs, config, pkgs, ... }: {
   imports = [ inputs.mealie.nixosModules.default ];
 
-  services.mealie = {
+  services.mealie-nightly = {
     enable = true;
     package = inputs.mealie.packages.${pkgs.stdenv.hostPlatform.system}.mealie;
   };
 
   nginx.subdomain.mealie = {
-    "/".proxyPass = "http://127.0.0.1:${toString config.services.mealie.port}/";
+    "/".proxyPass =
+      "http://127.0.0.1:${toString config.services.mealie-nightly.port}/";
   };
 
   services.backups.mealie = {
-    paths = [ config.services.mealie.stateDir ];
+    paths = [ config.services.mealie-nightly.stateDir ];
     restic = { s3 = { }; };
   };
 }
