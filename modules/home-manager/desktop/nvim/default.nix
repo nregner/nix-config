173371359eb553340b4cf4835703e9218ed2ee21
Nathan Regner <nathan.regner@clickbank.com>
2024-03-13 17:08:19 -0600

    wip(nvim): add jdtls

--- modules/home-manager/desktop/nvim/default.nix
+++ modules/home-manager/desktop/nvim/default.nix
@@ -7,6 +7,23 @@
     defaultEditor = true;
     extraConfig = builtins.readFile ./init.vim;
     extraLuaConfig = ''
+      vim.g.lombok_jar = '${
+        pkgs.fetchurl {
+          url = "https://repo1.maven.org/maven2/org/projectlombok/lombok/1.18.34/lombok-1.18.34.jar";
+          sha256 = "06mqsj33x0hxxd73gxw05i1np7khhfqwdg7w0wdis92nzwm6nzf2";
+        }
+      }'
+      vim.g.java_home = '${pkgs.jdk21_headless}'
+      vim.g.java_runtimes = {
+        {
+          name = "JavaSE-11",
+          path = "${pkgs.jdk11_headless}",
+        },
+        {
+          name = "JavaSE-17",
+          path = "${pkgs.jdk17_headless}",
+        },
+      }
       require('user')
     '';
 
@@ -19,6 +36,7 @@
       gopls
       libclang
       helm-ls
+      jdt-language-server
       lua-language-server
       nil
       nodePackages_latest.typescript
