3a10a31e30fa0420091f36fce26eb11f954c86de
Nathan Regner <nathan.regner@clickbank.com>
2024-03-14 11:04:13 -0600

    feat(nvim): add adaptive width nvim-tree keybinding

--- modules/home-manager/desktop/nvim/lua/user/plugins/nvim-tree.lua
+++ modules/home-manager/desktop/nvim/lua/user/plugins/nvim-tree.lua
@@ -4,6 +4,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local view_width_max = 30
+
 return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
@@ -22,5 +24,37 @@ return {
   opts = {
     update_cwd = true,
     hijack_cursor = true,
+    view = {
+      width = {
+        max = function()
+          return view_width_max
+        end,
+      },
+    },
+    on_attach = function(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function opts(desc)
+        return {
+          desc = "nvim-tree: " .. desc,
+          buffer = bufnr,
+          noremap = true,
+          silent = true,
+          nowait = true,
+        }
+      end
+
+      api.config.mappings.default_on_attach(bufnr)
+
+      vim.keymap.set("n", "A", function()
+        print("view_width_max", view_width_max)
+        if view_width_max == -1 then
+          view_width_max = 30
+        else
+          view_width_max = -1
+        end
+        api.tree.reload()
+      end, opts("Toggle Adaptive Width"))
+    end,
   },
 }
