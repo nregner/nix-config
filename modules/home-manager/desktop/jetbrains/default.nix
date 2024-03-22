bf254ad77f393095d602e8c5fba8676c33802d9e
Nathan Regner <nathan.regner@clickbank.com>
2024-03-22 12:27:55 -0600

    feat(home): add idea configs

--- modules/home-manager/desktop/jetbrains/default.nix
+++ modules/home-manager/desktop/jetbrains/default.nix
@@ -1,4 +1,36 @@
-{ config, ... }:
+{ config, lib, ... }:
+let
+  listFilesRecursive =
+    root:
+    builtins.map (file: {
+      name = (lib.removePrefix "./" (toString (lib.path.removePrefix (root) file)));
+      path = file;
+    }) (lib.filesystem.listFilesRecursive root);
+
+  linkConfigFiles =
+    appName:
+    let
+      commonConfig = (listFilesRecursive ./config/common);
+      appConfig = (listFilesRecursive (./config + "/${appName}"));
+    in
+    builtins.map (
+      { name, path }:
+      {
+        "JetBrains/${appName}/config/${name}" = {
+          source = config.lib.file.mkFlakeSymlink path;
+        };
+      }
+    ) (commonConfig ++ appConfig);
+in
 {
-  home.file.".ideavimrc".source = config.lib.file.mkFlakeSymlink ./ideavimrc;
+  config = {
+    home.file.".ideavimrc".source = config.lib.file.mkFlakeSymlink ./ideavimrc;
+
+    xdg.configFile = lib.mkMerge (
+      builtins.concatMap linkConfigFiles [
+        "idea"
+        "datagrip"
+      ]
+    );
+  };
 }
