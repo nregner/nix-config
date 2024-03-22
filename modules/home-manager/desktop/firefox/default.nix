709a73349446d7f6519df884246deb69c758bca5
Nathan Regner <nathan.regner@clickbank.com>
2024-03-22 12:27:55 -0600

    feat(home): add idea configs

--- modules/home-manager/desktop/firefox/default.nix
+++ modules/home-manager/desktop/firefox/default.nix
@@ -2,6 +2,7 @@
   programs.firefox = {
     profiles.default = {
 
+      # about:debugging#/runtime/this-firefox
       # extensions = [
       #   (pkgs.stdenv.mkDerivation {
       #     name = "firefox-extension-ublock-origin";
