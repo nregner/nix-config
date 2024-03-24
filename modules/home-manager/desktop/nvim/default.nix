0d0029e9bbb775dcfba7117eb46931a6b3d0456b
Nathan Regner <nathanregner@gmail.com>
2024-03-23 20:29:21 -0600

    feat: nvim-nightly

--- modules/home-manager/desktop/nvim/default.nix
+++ modules/home-manager/desktop/nvim/default.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, ... }: {
+{ inputs, config, pkgs, ... }: {
+  nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ];
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
