65ad3ebb3f0b6e4ba42497dc21702d1bbbf708bd
Nathan Regner <nathanregner@gmail.com>
2024-03-27 18:37:12 -0600

    fix(nvim): remap macro recorder to <leader>q

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -954,11 +954,15 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Remap record macro to prevent accidental presses
+vim.keymap.set("n", "<leader>q", "q", { noremap = true })
+vim.keymap.set("n", "q", "<nop>", { noremap = true })
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 vim.keymap.set("n", "<leader>sv", function()
   -- source: https://github.com/creativenull
