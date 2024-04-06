4572b0c5f6f11c4deab9443abcc4b9e99de61c2d
Nathan Regner <nathanregner@gmail.com>
2024-04-05 18:58:17 -0600

    fix(darwin): enable sandbox

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -1,4 +1,4 @@
-{
+{ config, lib, ... }: {
   imports = [ ../nixos/base/nix.nix ../nixos/desktop/nix.nix ];
 
   nix.gc = {
@@ -6,4 +6,13 @@
     options = "--delete-older-than 7d";
     user = "root";
   };
+
+  # https://github.com/NixOS/nix/issues/4119#issuecomment-1734738812
+  nix.settings.sandbox = "relaxed";
+  system.systemBuilderArgs =
+    lib.mkIf (config.nix.settings.sandbox == "relaxed") {
+      sandboxProfile = ''
+        (allow file-read* file-write* process-exec mach-lookup (subpath "${builtins.storeDir}"))
+      '';
+    };
 }
