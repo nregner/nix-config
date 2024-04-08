2b9dc4171091c514ca6ae14401f01128cf2d8a8b
Nathan Regner <nathan.regner@clickbank.com>
2024-04-08 17:43:30 -0600

    fix(home): fix k9s config path on darwin and get rid of IFD

--- modules/home-manager/desktop/cli/k9s.nix
+++ modules/home-manager/desktop/cli/k9s.nix
@@ -1,8 +1,17 @@
-{ inputs, config, ... }: {
-  programs.k9s = {
-    enable = true;
-    settings.ui.skin = "catppuccin-mocha";
-    skins.catppuccin-mocha = config.lib.formats.fromYAML
-      "${inputs.catppuccin-k9s}/dist/catppuccin-mocha.yaml";
-  };
+{ inputs, config, pkgs, ... }:
+let
+  yamlFormat = pkgs.formats.yaml { };
+  configDir = if (pkgs.stdenv.isDarwin) then
+    "Library/Application Support/k9s"
+  else
+    "${config.xdg.configHome}/k9s";
+in {
+  home.packages = [ pkgs.unstable.k9s ];
+
+  home.file."${configDir}/config.yaml".source =
+    yamlFormat.generate "k9s-config" { k9s.ui.skin = "catppuccin"; };
+
+  home.file."${configDir}/skins/catppuccin.yaml".source =
+    "${inputs.catppuccin-k9s}/dist/catppuccin-mocha.yaml";
+
 }
