dfaf3156c8414e319a0e1497f70e24dfb736ed98
Nathan Regner <nathanregner@gmail.com>
2024-04-21 14:53:16 -0600

    fix(sagittarius): allow user access to hydra postgres

--- machines/sagittarius/services/hydra.nix
+++ machines/sagittarius/services/hydra.nix
@@ -32,6 +32,10 @@
     '';
   };
 
+  services.postgresql.identMap = ''
+    hydra-users nregner hydra
+  '';
+
   sops.secrets.hydra-github-webhook-secret = {
     key = "hydra/github_webhook_secret";
     owner = "hydra-sentinel-server";
