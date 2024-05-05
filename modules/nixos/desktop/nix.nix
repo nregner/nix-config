89a460e4d981f4fcc42b7f085e4796a79cba46ff
Nathan Regner <nathanregner@gmail.com>
2024-05-04 19:18:50 -0600

    fix: correct invalid flake registry entries

--- modules/nixos/desktop/nix.nix
+++ modules/nixos/desktop/nix.nix
@@ -1,8 +1,8 @@
 { inputs, ... }: {
   nix.registry = {
     # pin the flake registry to inputs to avoid extra downloads
-    nixpkgs.to = inputs.nixpkgs;
-    nixpkgs-stable.to = inputs.nixpkgs;
+    nixpkgs.flake = inputs.nixpkgs;
+    nixpkgs-stable.flake = inputs.nixpkgs;
 
     # also pin a "github" entry for use of `nix flake lock --override-input nixpkgs "flake:nixpkgs-git"` in other flakes
     # unlike the above entries, this won't result in a store path in the lockfile
