a8b7637bca93c5b9d5e6d4a0e3c9290250fcb2e7
Nathan Regner <nathanregner@gmail.com>
2024-05-11 15:14:33 -0600

    fix(nvim): allow code-actions to be triggered in visual mode

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -131,25 +131,21 @@ require("lazy").setup({
       { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-      -- [[ Configure LSP ]]
-      --  This function gets run when an LSP connects to a particular buffer.
       local on_attach = function(_, bufnr)
-        -- NOTE: Remember that lua is a real programming language, and as such it is possible
-        -- to define small helper and utility functions so you don't have to repeat yourself
-        -- many times.
-        --
-        -- In this case, we create a function that lets us more easily define mappings specific
-        -- for LSP related items. It sets the mode, buffer and description for us each time.
-        local nmap = function(keys, func, desc)
+        local map = function(mode, keys, func, desc)
           if desc then
             desc = "LSP: " .. desc
           end
 
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
+        end
+
+        local nmap = function(keys, func, desc)
+          map("n", keys, func, desc)
         end
 
         nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
         nmap("<leader>fci", vim.lsp.buf.incoming_calls, "[F]ind [C]allers [I]ncoming")
         nmap("<leader>fca", vim.lsp.buf.outgoing_calls, "[F]ind [C]allers [O]outgoing")
 
