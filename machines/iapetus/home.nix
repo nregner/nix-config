8c608223cd32093976e10d77aa0d1637dc54c8d8
Nathan Regner <nathanregner@gmail.com>
2024-05-17 20:35:39 -0600

    fix(iapetus): use mold linker for cargo projects

--- machines/iapetus/home.nix
+++ machines/iapetus/home.nix
@@ -53,10 +53,14 @@
   ];
 
   # rustc -Z unstable-options --print target-spec-json | jq '.["llvm-target"]' -r
+  # https://github.com/rui314/mold?tab=readme-ov-file#how-to-use
+  # https://discourse.nixos.org/t/create-nix-develop-shell-for-rust-with-mold/35894/6
   home.file.".cargo/config.toml".source = pkgs.writeText "config.toml" ''
     [target.x86_64-unknown-linux-gnu]
-    linker = "${pkgs.clang}/bin/clang"
-    rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.unstable.mold}/bin/mold", "-C", "target-cpu=native"]
+    linker = "${
+      (lib.getExe' ((pkgs.unstable.stdenvAdapters.useMoldLinker
+        pkgs.unstable.clangStdenv).cc) "clang")
+    }"
   '';
 
   programs.alacritty.settings = { font = { size = lib.mkForce 11; }; };
