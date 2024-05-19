93261c43ddd57f6fa62ae23596664c20985e4537
Nathan Regner <nathanregner@gmail.com>
2024-05-18 20:17:01 -0600

    feat(nixos): enable system.autoUpgrade

--- modules/nixos/server/default.nix
+++ modules/nixos/server/default.nix
@@ -1,5 +1,14 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
   imports = [ ../base ./services ./programs ];
   services.tailscaled-autoconnect.enable = lib.mkDefault true;
   services.nregner.metrics.enable = lib.mkDefault true;
+
+  system.autoUpgrade = {
+    enable = true;
+    dates = "*-*-* *:20:00";
+    flake = "github:nathanregner/nix-config#${config.networking.hostName}";
+    operation = "boot";
+    flags = [ "--refresh" ];
+    randomizedDelaySec = "5m";
+  };
 }
