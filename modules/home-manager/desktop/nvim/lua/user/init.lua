1fdf79fee34b9a6168b8e3f6f9749287cec1395d
Nathan Regner <nathan.regner@clickbank.com>
2024-05-22 16:39:40 -0600

    feat(nvim): set local plugin path

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
   -- https://github.com/sindrets/diffview.nvim#configuration
   {
     "sindrets/diffview.nvim",
+    dev = true,
     opts = {
       view = {
         merge_tool = {
