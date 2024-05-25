894f07eba5714c35e438c82dcc2512a594421c3d
Nathan Regner <nathanregner@gmail.com>
2024-05-24 18:09:32 -0600

    feat(nvim): add colorizer

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -917,6 +917,25 @@ require("lazy").setup({
     end,
   },
 
+  { -- Colorizer
+    "norcalli/nvim-colorizer.lua",
+    event = "VeryLazy",
+    cmd = "ColorizerToggle",
+    opts = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = false, -- "Name" codes like Blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    },
+    config = function(_, opts)
+      require("colorizer").setup({ "*" }, opts)
+    end,
+  },
+
   { -- Neotest
     "nvim-neotest/neotest",
     dependencies = {
