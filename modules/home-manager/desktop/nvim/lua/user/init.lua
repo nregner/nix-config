139fe4c7bcf09e4d76cc01b139605dba7050cfa6
Nathan Regner <nathanregner@gmail.com>
2024-06-14 20:02:15 -0600

    wip: feat(nvim): add multicursors.nvim

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -1114,6 +1114,24 @@ require("lazy").setup({
     },
   },
 
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "smoka7/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
+
   { -- REPL
     "Olical/conjure",
     ft = { "clojure", "lua" },
