08924bef93823e058e600c6a11e88fe6cd105757
Nathan Regner <nathanregner@gmail.com>
2024-06-19 18:51:11 -0600

    tweak(nvim): use default gitsigns config

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -523,14 +523,6 @@ require("lazy").setup({
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
     opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
