70451d8bee57b676ff097ec97396aad4ec3a1129
Nathan Regner <nathan.regner@clickbank.com>
2024-08-05 17:14:13 -0600

    fixup! feat(home): replace starship with spaceship (async zsh prompt)

--- modules/home-manager/desktop/cli/spaceshiprc.zsh
+++ modules/home-manager/desktop/cli/spaceshiprc.zsh
@@ -1,6 +1,12 @@
 # Display username always
 SPACESHIP_USER_SHOW=never
 
+# https://spaceship-prompt.sh/config/intro/
+SPACESHIP_DOCKER_SHOW=false
+SPACESHIP_DOCKER_COMPOSE_SHOW=false
+
+# TODO
 # Add a custom vi-mode section to the prompt
 # See: https://github.com/spaceship-prompt/spaceship-vi-mode
-spaceship add --before char vi_mode
+# spaceship add --before char vi_mode
+
