9717ca0a9c8807dd537862e977c275d6d8bcc383
Nathan Regner <nathanregner@gmail.com>
2024-08-09 12:07:39 -0700

    refactor: update simplify darwin builder

--- machines/enceladus/linux-builder/configuration.nix
+++ machines/enceladus/linux-builder/configuration.nix
@@ -1,19 +1,14 @@
 { self, lib, ... }:
 {
-  imports = [ ../../../modules/nixos/server ];
+  imports = [ ../../../modules/nixos/base ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
 
   networking.hostName = "enceladus-linux-vm";
 
-  # FIXME: Have to manually run /run/current-system/activate to get secrets to show up...
-  sops.defaultSopsFile = null;
-
   users.users = {
     nregner.openssh.authorizedKeys.keys = lib.attrValues self.globals.ssh.allKeys;
   };
-
-  services.tailscaled-autoconnect.enable = true;
 
   # don't rebuild vm image on every commit
   system.nixos.tags = lib.mkForce [ ];
