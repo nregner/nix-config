3c4d6c38142d1acadaa947d32b9c273e696a4b04
Nathan Regner <nathanregner@gmail.com>
2024-08-14 13:06:35 -0600

    build: update inputs

--- machines/sagittarius/services/mealie.nix
+++ machines/sagittarius/services/mealie.nix
@@ -5,21 +5,17 @@ in
 {
   services.mealie = {
     enable = true;
-    package =
-      (pkgs.unstable.mealie.override {
-        # FIXME: https://github.com/NixOS/nixpkgs/issues/325120
-        python3Packages = pkgs.unstable.python311.pkgs;
-      }).overrideAttrs
-        (old: {
-          # FIXME: https://github.com/NixOS/nixpkgs/issues/321623
-          patches = (old.patches or [ ]) ++ [
-            (pkgs.fetchpatch {
-              url = "https://github.com/mealie-recipes/mealie/commit/445754c5d844ccf098f3678bc4f3cc9642bdaad6.patch";
-              hash = "sha256-ZdATmSYxhGSjoyrni+b5b8a30xQPlUeyp3VAc8OBmDY=";
-              revert = true;
-            })
-          ];
-        });
+    package = pkgs.unstable.mealie.overrideAttrs (old: {
+      # FIXME: https://github.com/NixOS/nixpkgs/issues/321623
+      patches = (old.patches or [ ]) ++ [
+        (pkgs.fetchpatch {
+          url = "https://github.com/mealie-recipes/mealie/commit/445754c5d844ccf098f3678bc4f3cc9642bdaad6.patch";
+          hash = "sha256-ZdATmSYxhGSjoyrni+b5b8a30xQPlUeyp3VAc8OBmDY=";
+          revert = true;
+        })
+      ];
+      doCheck = false;
+    });
   };
 
   nginx.subdomain.mealie = {
