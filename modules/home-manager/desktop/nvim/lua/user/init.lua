2f3a55e673458d6870e32ff1bf2a1442be84ae7f
Nathan Regner <nathan.regner@clickbank.com>
2024-08-15 08:43:24 -0600

    disable colorizer

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -1422,26 +1422,26 @@ require("lazy").setup({
     },
   },
 
-  { -- Colorizer
-    "norcalli/nvim-colorizer.lua",
-    event = "VeryLazy",
-    cmd = "ColorizerToggle",
-    opts = {
-      "*", -- Highlight all files
-      "!TelescopePrompt", -- Except telescope previews. Seems to result in freezes: https://github.com/nvim-telescope/telescope.nvim/issues/1379
-      RGB = true, -- #RGB hex codes
-      RRGGBB = true, -- #RRGGBB hex codes
-      names = false, -- "Name" codes like Blue
-      RRGGBBAA = true, -- #RRGGBBAA hex codes
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    },
-    config = function(_, opts)
-      require("colorizer").setup({ "*" }, opts)
-    end,
-  },
+  -- { -- Colorizer
+  --   "norcalli/nvim-colorizer.lua",
+  --   event = "VeryLazy",
+  --   cmd = "ColorizerToggle",
+  --   opts = {
+  --     "*", -- Highlight all files
+  --     "!TelescopePrompt", -- Except telescope previews. Seems to result in freezes: https://github.com/nvim-telescope/telescope.nvim/issues/1379
+  --     RGB = true, -- #RGB hex codes
+  --     RRGGBB = true, -- #RRGGBB hex codes
+  --     names = false, -- "Name" codes like Blue
+  --     RRGGBBAA = true, -- #RRGGBBAA hex codes
+  --     rgb_fn = true, -- CSS rgb() and rgba() functions
+  --     hsl_fn = true, -- CSS hsl() and hsla() functions
+  --     css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  --     css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  --   },
+  --   config = function(_, opts)
+  --     require("colorizer").setup({ "*" }, opts)
+  --   end,
+  -- },
 
   { -- Neotest
     "nvim-neotest/neotest",
