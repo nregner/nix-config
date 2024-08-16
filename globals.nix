b61588c9a235695f3e80850cc64a00e053a7992d
Nathan Regner <nathanregner@gmail.com>
2024-08-15 18:01:52 -0600

    fix: update enceladus host key

--- globals.nix
+++ globals.nix
@@ -3,7 +3,7 @@
 {
   ssh = rec {
     hostKeys = {
-      enceladus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKs6KBP0vkY+EHrtZvIq9KsWGQ83iet0Enu7AA1nhyAP";
+      enceladus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIByCYpEo+AjtM2kWxxr5C9Mp3tm1PyVDVD8BGesKTi85";
       enceladus-linux-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBWcxb/Blaqt1auOtE+F8QUWrUotiC5qBJ+UuEWdVCb";
       iapetus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhre0L0AW87qYkI5Os8U2+DS5yvAOnjpEY+Lmn5f0l7";
       sagittarius = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQOaeRY07hRIPpeFYRWoQOzP+toxZjveC5jVHF+vpIj";
