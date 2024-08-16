23a912c61d33c8012b388528251e1f4b24c93d75
Nathan Regner <nathanregner@gmail.com>
2024-08-15 18:01:52 -0600

    fix: update enceladus host key

--- globals.nix
+++ globals.nix
@@ -2,9 +2,10 @@
 { lib }:
 {
   ssh = rec {
-    hostKeys = {
-      enceladus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKs6KBP0vkY+EHrtZvIq9KsWGQ83iet0Enu7AA1nhyAP";
+    hostKeys = rec {
+      enceladus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIByCYpEo+AjtM2kWxxr5C9Mp3tm1PyVDVD8BGesKTi85";
       enceladus-linux-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBWcxb/Blaqt1auOtE+F8QUWrUotiC5qBJ+UuEWdVCb";
+      "[localhost]:31022" = enceladus-linux-vm;
       iapetus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhre0L0AW87qYkI5Os8U2+DS5yvAOnjpEY+Lmn5f0l7";
       sagittarius = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQOaeRY07hRIPpeFYRWoQOzP+toxZjveC5jVHF+vpIj";
       voron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbDjUMsVH2t2f+pldWmU23ahMShVIlws1icrn66Jexu";
@@ -14,7 +15,7 @@
       "nregner@enceladus" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJN0UxPvRjkqYdq8OFtzO/borc4lU4QNYSJiGhgx3MkI";
       "nregner@iapetus" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDk7uVEehfyhAZUvZbvH5Kw85MzLyYqVdTOMBXsmBeLx";
       "nregner@sagittarius" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIArpJ0oIqZ8amBOGjwPSoxAXMzgNeyu8fV9pfQmsGl+i";
-      "hydra@sagittarius" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8n6H94elpMmRgK3oTt5bAL3XMiDgJejFVUXsWgQ8XK hydra@sagittarius";
+      "hydra@sagittarius" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8n6H94elpMmRgK3oTt5bAL3XMiDgJejFVUXsWgQ8XK";
     };
 
     allKeys = hostKeys // userKeys;
