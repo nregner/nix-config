6a0bc0ea4a485374c2bbae3825cf902162387d2a
Nathan Regner <nathanregner@gmail.com>
2024-08-28 20:42:47 -0600

    perf(nvim): disable treesitter on large files

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -933,6 +933,16 @@ require("lazy").setup({
       auto_install = true,
       highlight = { enable = true },
       -- indent = { enable = true },
+      disable = function(_lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+        if #vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] > 1000 then
+          return true
+        end
+      end,
       incremental_selection = {
         enable = true,
         keymaps = {
