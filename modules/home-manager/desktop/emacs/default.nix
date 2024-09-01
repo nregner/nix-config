759c03e7449f8109100e213f62d3c35200bfb232
Nathan Regner <nathanregner@gmail.com>
2024-09-01 12:11:18 -0600

    fixup! fixup! wip: feat(home): add doom emacs

--- modules/home-manager/desktop/emacs/default.nix
+++ modules/home-manager/desktop/emacs/default.nix
@@ -4,22 +4,25 @@
   ...
 }:
 {
-  imports = [ inputs.nix-doom-emacs-unstraightened.hmModule ];
+  # imports = [ inputs.nix-doom-emacs-unstraightened.hmModule ];
 
-  programs.doom-emacs = {
-    enable = true;
-    # doomDir = config.lib.file.mkFlakeSymlink "./doom";
-    doomDir = ./doom;
-    # doomLocalDir = "~/.local/share/nix-doom";
-    extraPackages = epkgs: [ epkgs.treesit-grammars.with-all-grammars ];
-  };
+  # programs.doom-emacs = {
+  #   enable = true;
+  #   # doomDir = config.lib.file.mkFlakeSymlink "./doom";
+  #   doomDir = ./doom;
+  #   # doomLocalDir = "~/.local/share/nix-doom";
+  #   extraPackages = epkgs: [ epkgs.treesit-grammars.with-all-grammars ];
+  # };
 
-  services.emacs = {
-    enable = true;
-    socketActivation.enable = true;
-  };
+  # services.emacs = {
+  #   enable = true;
+  #   socketActivation.enable = true;
+  # };
 
-  xdg.configFile."emacs2".source = config.lib.file.mkFlakeSymlink ./user;
+  # xdg.configFile."emacs2".source = config.lib.file.mkFlakeSymlink ./user;
+
+  programs.emacs.enable = true;
+
   xdg.configFile."doom/config.el".source = ./user/config.el;
   xdg.configFile."doom/init.el".source = ./doom/init.el;
   xdg.configFile."doom/packages.el".source = ./doom/packages.el;
