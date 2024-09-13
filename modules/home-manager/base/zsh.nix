ed652c3db884e87cdcffb547868d7fc5937a9039
Nathan Regner <nathan.regner@clickbank.com>
2024-09-13 15:50:47 -0600

    fix(zsh): incorrect snr alias on darwin

--- modules/home-manager/base/zsh.nix
+++ modules/home-manager/base/zsh.nix
@@ -26,7 +26,7 @@
 
         nr = "${nixRebuild} --flake .";
         nrb = "${nr} build";
-        snr = "${nr} --use-remote-sudo";
+        snr = if pkgs.stdenv.isDarwin then "sudo ${nr}" else "${nr} --use-remote-sudo";
         snrb = "${snr} boot";
         snrs = "${snr} switch";
         snrt = "${snr} test";
