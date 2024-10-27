5e852003783fb0464691f26b3478485ec29846fc
Nathan Regner <nathanregner@gmail.com>
2024-10-27 12:03:36 -0600

    fixup! feat(sagittarius): add factorio user

--- machines/sagittarius/users.nix
+++ machines/sagittarius/users.nix
@@ -9,4 +9,10 @@
   networking.firewall = {
     allowedUDPPorts = [ 34197 ];
   };
+
+  users.users.craigslist = {
+    isNormalUser = true;
+    extraGroups = [ "docker" ];
+    openssh.authorizedKeys.keys = builtins.attrValues self.globals.ssh.userKeys.nregner;
+  };
 }
