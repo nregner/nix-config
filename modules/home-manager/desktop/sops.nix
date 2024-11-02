235c01af0c93a6e872b513b1598ba40d388857d7
Nathan Regner <nathanregner@gmail.com>
2024-11-02 11:41:55 -0600

    feat(home): enable sops.age.generateKey

--- modules/home-manager/desktop/sops.nix
+++ modules/home-manager/desktop/sops.nix
@@ -7,10 +7,14 @@
 }:
 {
   imports = [ inputs.sops-nix.homeManagerModule ];
-  sops.age.keyFile = lib.mkDefault "${
-    if pkgs.stdenv.isDarwin then
-      "${config.home.homeDirectory}/Library/Application Support"
-    else
-      config.xdg.configHome
-  }/sops/age/keys.txt";
+  sops.age = {
+    keyFile = lib.mkDefault "${
+      if pkgs.stdenv.isDarwin then
+        "${config.home.homeDirectory}/Library/Application Support"
+      else
+        config.xdg.configHome
+    }/sops/age/keys.txt";
+    sshKeyPaths = lib.mkDefault [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+    generateKey = true;
+  };
 }
