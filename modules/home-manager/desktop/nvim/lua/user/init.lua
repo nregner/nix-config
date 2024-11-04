7724b2c74180cb7ccccf19e1c06e7f90fa2b5629
Nathan Regner <nathan.regner@clickbank.com>
2024-11-04 14:45:53 -0700

    feat(nvim): add tflint

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -927,7 +927,7 @@ require("lazy").setup({
     opts = {
       auto_install = true,
       highlight = { enable = true },
-      -- indent = { enable = true },
+      indent = { enable = true },
       disable = function(_lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
