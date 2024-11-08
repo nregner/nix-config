b74e85950eb588757457a5b4abfc3ef967ef597d
Nathan Regner <nathanregner@gmail.com>
2024-11-07 17:31:14 -0700

    fix(sagittarius): enable user lingering

--- machines/sagittarius/users.nix
+++ machines/sagittarius/users.nix
@@ -1,9 +1,14 @@
-{ self, ... }:
+{
+  self,
+  pkgs,
+  lib,
+  ...
+}:
 {
   users.users.factorio = {
     isNormalUser = true;
-    extraGroups = [ "docker" ];
     openssh.authorizedKeys.keys = builtins.attrValues self.globals.ssh.userKeys.nregner;
+    linger = true;
   };
 
   networking.firewall = {
@@ -14,5 +19,12 @@
     isNormalUser = true;
     extraGroups = [ "docker" ];
     openssh.authorizedKeys.keys = builtins.attrValues self.globals.ssh.userKeys.nregner;
+    linger = true;
   };
+
+  # https://discourse.nixos.org/t/nixos-rebuild-switch-is-failing-when-systemd-linger-is-enabled/31937/5
+  systemd.user.services.nixos-activation.unitConfig.ConditionUser = lib.mkForce [
+    "!craigslist"
+    "!factorio"
+  ];
 }
