c4bf6822af902f379197eaab0efd8d4aefcd806c
Nathan Regner <nathanregner@gmail.com>
2024-12-14 11:11:07 -0700

    feat(nvim): use clippy instead of cargo check

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -303,7 +303,12 @@ require("lazy").setup({
         rust_analyzer = {
           -- https://rust-analyzer.github.io/manual.html#configuration
           ["rust-analyzer"] = {
-            cargo = { allFeatures = true },
+            cargo = {
+              allFeatures = true,
+            },
+            check = {
+              command = "clippy",
+            },
             completion = {
               autoimport = { enable = true },
             },
