4f20153f9dc338e710e5b0ed870f9d089f6f0a4f
Nathan Regner <nathanregner@gmail.com>
2024-12-15 10:53:35 -0700

    chore: remove moonraker patches

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,10 +16,6 @@ let
     rec {
       inherit (inputs.clojure-lsp.packages.${final.system}) clojure-lsp;
 
-      # FIXME: hack to bypass "FATAL: Module ahci not found" error
-      # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
-      makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
-
       hydra_unstable = prev.hydra_unstable.overrideAttrs (oldAttrs: {
         patches = (oldAttrs.patches or [ ]) ++ [
           ./hydra/fix-restrict-eval-does-not-allow-access-to-git-flake.patch
@@ -28,8 +24,9 @@ let
         checkPhase = "";
       });
 
-      # FIXME: https://github.com/NixOS/nixpkgs/issues/357979
-      moonraker = warnIfOutdated prev.moonraker (final.callPackage ./moonraker { });
+      # FIXME: hack to bypass "FATAL: Module ahci not found" error
+      # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+      makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
 
       nix-fast-build = inputs.nix-fast-build.packages.${final.system}.default;
 
