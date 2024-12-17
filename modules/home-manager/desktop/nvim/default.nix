0dc626069743e613d0f39379457bd9414ea796f4
Nathan Regner <nathanregner@gmail.com>
2024-12-16 19:22:13 -0700

    feat(nvim): sync lazy plugins on activation

--- modules/home-manager/desktop/nvim/default.nix
+++ modules/home-manager/desktop/nvim/default.nix
@@ -45,6 +45,10 @@
     ];
   };
 
+  home.activation.lazy-sync = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    ${config.programs.neovim.finalPackage}/bin/nvim --headless "+Lazy! restore" +qa
+  '';
+
   home.packages = with pkgs.unstable; [
     # test runners
     cargo-nextest # for rouge8/neotest-rust
