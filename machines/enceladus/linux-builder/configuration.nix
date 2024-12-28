3c659b346ceffd01f641d01770822264bfa196a4
Nathan Regner <nathanregner@gmail.com>
2024-12-27 18:40:39 -0700

    feat(enceladus): don't create store image on every boot of builder vm

--- machines/enceladus/linux-builder/configuration.nix
+++ machines/enceladus/linux-builder/configuration.nix
@@ -1,21 +1,17 @@
 { self, lib, ... }:
 {
-  imports = [ ../../../modules/nixos/server ];
+  imports = [
+    ../../../modules/nixos/base/nix.nix
+    ./store-image.nix
+  ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
 
   networking.hostName = "enceladus-linux-vm";
 
-  # FIXME: Have to manually run /run/current-system/activate to get secrets to show up...
-  sops.defaultSopsFile = null;
-
   users.users = {
-    nregner.openssh.authorizedKeys.keys = lib.attrValues self.globals.ssh.allKeys;
+    builder.openssh.authorizedKeys.keys = lib.attrValues self.globals.ssh.allKeys;
   };
-
-  services.tailscaled-autoconnect.enable = true;
-
-  system.hydra-auto-upgrade.enable = false;
 
   # don't rebuild vm image on every commit
   system.nixos.tags = lib.mkForce [ ];
