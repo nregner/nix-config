5f4eab467736fb0327d239d29a94ddabafc1386c
Nathan Regner <nathan.regner@clickbank.com>
2025-01-06 16:09:58 -0700

    fixup! refactor: misc 24.11 cleanup

--- modules/nixos/desktop/nix.nix
+++ modules/nixos/desktop/nix.nix
@@ -1,35 +1,33 @@
 { inputs, ... }:
 {
-  config = {
-    nix.registry = {
-      # pin the flake registry to inputs to avoid extra downloads
-      nixpkgs.flake = inputs.nixpkgs;
-      nixpkgs-stable.flake = inputs.nixpkgs;
+  nix.registry = {
+    # pin the flake registry to inputs to avoid extra downloads
+    nixpkgs.flake = inputs.nixpkgs;
+    nixpkgs-stable.flake = inputs.nixpkgs;
 
-      # also pin a "github" entry for use of `nix flake lock --override-input nixpkgs "flake:nixpkgs-git"` in other flakes
-      # unlike the above entries, this won't result in a store path in the lockfile
-      nixpkgs-git.to = {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = inputs.nixpkgs-unstable.rev;
-        type = "github";
-      };
-      nixpkgs-stable-git.to = {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = inputs.nixpkgs.rev;
-        type = "github";
-      };
+    # also pin a "github" entry for use of `nix flake lock --override-input nixpkgs "flake:nixpkgs-git"` in other flakes
+    # unlike the above entries, this won't result in a store path in the lockfile
+    nixpkgs-git.to = {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = inputs.nixpkgs-unstable.rev;
+      type = "github";
     };
-
-    nix.settings = {
-      # keep build dependencies for direnv GC roots
-      keep-derivations = true;
-      keep-outputs = true;
-
-      # https://discourse.nixos.org/t/do-flakes-also-set-the-system-channel/19798
-      # pin system channels to flake inputs
-      nix-path = "nixpkgs=${inputs.nixpkgs-unstable}";
+    nixpkgs-stable-git.to = {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = inputs.nixpkgs.rev;
+      type = "github";
     };
+  };
+
+  nix.settings = {
+    # keep build dependencies for direnv GC roots
+    keep-derivations = true;
+    keep-outputs = true;
+
+    # https://discourse.nixos.org/t/do-flakes-also-set-the-system-channel/19798
+    # pin system channels to flake inputs
+    nix-path = "nixpkgs=${inputs.nixpkgs-unstable}";
   };
 }
