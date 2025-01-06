371801ff2108981a14398ebb58d6a23ae80dd6c8
Nathan Regner <nathan.regner@clickbank.com>
2025-01-06 16:14:13 -0700

    fixup! fixup! refactor: misc 24.11 cleanup

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -1,4 +1,4 @@
-{ outputs, ... }:
+{ inputs, outputs, ... }:
 {
   imports = [
     ../base
@@ -13,6 +13,27 @@
 
   # standalone install - reimport nixpkgs
   nixpkgs = import ../../../nixpkgs.nix { inherit outputs; };
+
+  nix.registry = {
+    # pin the flake registry to inputs to avoid extra downloads
+    nixpkgs.flake = inputs.nixpkgs;
+    nixpkgs-stable.flake = inputs.nixpkgs;
+
+    # also pin a "github" entry for use of `nix flake lock --override-input nixpkgs "flake:nixpkgs-git"` in other flakes
+    # unlike the above entries, this won't result in a store path in the lockfile
+    nixpkgs-git.to = {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = inputs.nixpkgs-unstable.rev;
+      type = "github";
+    };
+    nixpkgs-stable-git.to = {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = inputs.nixpkgs.rev;
+      type = "github";
+    };
+  };
 
   # Allow home-manager to manage itself
   programs.home-manager.enable = true;
