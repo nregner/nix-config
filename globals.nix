421a358f66f7c5937a8408e20f48f79d90b53d61
Nathan Regner <nathanregner@gmail.com>
2025-01-11 14:49:31 -0700

    build: update voron host keys

--- globals.nix
+++ globals.nix
@@ -7,7 +7,7 @@
       enceladus-linux-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBWcxb/Blaqt1auOtE+F8QUWrUotiC5qBJ+UuEWdVCb";
       iapetus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhre0L0AW87qYkI5Os8U2+DS5yvAOnjpEY+Lmn5f0l7";
       sagittarius = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQOaeRY07hRIPpeFYRWoQOzP+toxZjveC5jVHF+vpIj";
-      voron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbDjUMsVH2t2f+pldWmU23ahMShVIlws1icrn66Jexu";
+      voron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwRmpXQaCQ57F9KBmWAd5nPLSNh0gEro7i8JPDal8XL";
     };
 
     userKeys = {
