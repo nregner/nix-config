81c3162ab558e3b1b0089ee28adc9de4a29a4b46
Nathan Regner <nathanregner@gmail.com>
2025-01-10 18:52:13 -0700

    wip(sagittarius): switch to disko

--- machines/sagittarius/hardware-configuration.nix
+++ machines/sagittarius/hardware-configuration.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     "${modulesPath}/installer/scan/not-detected.nix"
+    inputs.disko.nixosModules.disko
     inputs.nixos-hardware.nixosModules.common-cpu-intel
     inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
@@ -35,64 +36,95 @@
     extraModulePackages = [ ];
   };
 
-  fileSystems."/" = {
+  disko.devices.disk.main = {
+    type = "disk";
     device = "/dev/disk/by-uuid/bed76273-18ca-45c6-9008-93a5332f7608";
-    fsType = "btrfs";
-    options = [
-      "subvol=@"
-      "noatime"
-    ];
+    content = {
+      type = "gpt";
+      partitions.boot = {
+        label = "nixos-boot";
+        type = "EF00";
+        size = "1G";
+        priority = 1;
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+        };
+      };
+      partitions.root = {
+        label = "nixos-root";
+        size = "100%";
+        priority = 2;
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ]; # Override existing partition
+          subvolumes = {
+            "@" = {
+              mountpoint = "/";
+              mountOptions = [ "noatime" ];
+            };
+            "@home" = {
+              mountpoint = "/home";
+              mountOptions = [ "noatime" ];
+            };
+            "@home-snapshots" = {
+              mountpoint = "/home/.snapshots";
+              mountOptions = [ "noatime" ];
+            };
+            "@nix" = {
+              mountpoint = "/nix";
+              mountOptions = [ "noatime" ];
+            };
+            "@var" = { };
+            "@var-lib" = {
+              mountpoint = "/var/lib";
+              mountOptions = [ "noatime" ];
+            };
+            "@var-log" = {
+              mountpoint = "/var/log";
+              mountOptions = [ "noatime" ];
+            };
+          };
+        };
+      };
+    };
   };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/bed76273-18ca-45c6-9008-93a5332f7608";
-    fsType = "btrfs";
-    options = [
-      "subvol=@home"
-      "noatime"
-    ];
-  };
-
-  fileSystems."/var/log" = {
-    device = "/dev/disk/by-uuid/bed76273-18ca-45c6-9008-93a5332f7608";
-    fsType = "btrfs";
-    options = [
-      "subvol=@var-lib"
-      "noatime"
-    ];
-  };
-
-  fileSystems."/var/lib" = {
-    device = "/dev/disk/by-uuid/bed76273-18ca-45c6-9008-93a5332f7608";
-    fsType = "btrfs";
-    options = [
-      "subvol=@var-log"
-      "noatime"
-    ];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/bed76273-18ca-45c6-9008-93a5332f7608";
-    fsType = "btrfs";
-    options = [
-      "subvol=@nix"
-      "noatime"
-    ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/A652-29E4";
-    fsType = "vfat";
-  };
-
-  fileSystems."/vol/data" = {
+  disko.devices.disk.data = {
+    type = "disk";
     device = "/dev/disk/by-uuid/394db84b-1663-4d06-9cfa-794c3162bd93";
-    fsType = "ext4";
+    content = {
+      type = "gpt";
+      partitions.root = {
+        label = "nixos-root";
+        size = "100%";
+        priority = 1;
+        content = {
+          type = "filesystem";
+          format = "ext4";
+          mountpoint = "/vol/data";
+        };
+      };
+    };
   };
 
-  fileSystems."/vol/backup" = {
+  disko.devices.disk.backup = {
+    type = "disk";
     device = "/dev/disk/by-uuid/dfe2646a-e49d-4d93-a118-7b1f1085db08";
-    fsType = "btrfs";
+    content = {
+      type = "gpt";
+      partitions.root = {
+        label = "backup";
+        size = "100%";
+        priority = 1;
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ]; # Override existing partition
+          mountpoint = "/vol/backup";
+        };
+      };
+    };
   };
 
   zramSwap.enable = true;
