a832f95c11062dfd832ef7e1c804609580125493
Nathan Regner <nathan.regner@clickbank.com>
2025-01-13 11:59:30 -0700

    misc

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -15,19 +15,13 @@
   nixpkgs = import ../../../nixpkgs.nix { inherit outputs; };
 
   nix.registry = {
-    # pin the flake registry to inputs to avoid extra downloads
-    nixpkgs.flake = inputs.nixpkgs;
-    nixpkgs-stable.flake = inputs.nixpkgs;
-
-    # also pin a "github" entry for use of `nix flake lock --override-input nixpkgs "flake:nixpkgs-git"` in other flakes
-    # unlike the above entries, this won't result in a store path in the lockfile
-    nixpkgs-git.to = {
+    nixpkgs.to = {
       owner = "NixOS";
       repo = "nixpkgs";
       rev = inputs.nixpkgs-unstable.rev;
       type = "github";
     };
-    nixpkgs-stable-git.to = {
+    nixpkgs-stable.to = {
       owner = "NixOS";
       repo = "nixpkgs";
       rev = inputs.nixpkgs.rev;
